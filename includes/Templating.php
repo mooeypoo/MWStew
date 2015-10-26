fbaedec50650a662dc1e754cd3a0142c2516cd27
Moriel Schottlender <moriel@gmail.com>
2015-10-26 11:17:35 -0700

    Clean up unnecessary files and add 'cache' directory

--- includes/Templating.php
+++ includes/Templating.php
@@ -5,11 +5,13 @@ namespace MWStew;
 class Templating {
 	protected $loader = null;
 	protected $twig = null;
-	protected $twigPath = BASE_PATH . '/includes/twig';
+	protected $twigPath;
 
 	public function __construct( $twigPath = '' ) {
 		if ( strlen( $twigPath ) > 0 ) {
 			$this->twigPath = $twigPath;
+		} else {
+			$this->twigPath = BASE_PATH . '/includes/twig';
 		}
 		$this->loader = new \Twig_Loader_Filesystem( $this->twigPath . '/templates' );
 		$this->twig = new \Twig_Environment( $this->loader, array(
