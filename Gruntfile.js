dee7580d826777faa6463d2dfb40c3f1c202bd61
Moriel Schottlender <moriel@gmail.com>
2016-03-19 16:29:33 -0700

    Add Gruntfile and change CSS to LESS
    Convert LESS back to CSS through grunt, and add validation for
    the resulting CSS. Also prepare the ground for language support
    with the option of RTLing with CSSJanus (currently inactive)

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-less' );
+	grunt.loadNpmTasks( 'grunt-contrib-csslint' );
+	// grunt.loadNpmTasks( 'grunt-cssjanus' );
+
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		less: {
+			site: {
+				files: {
+					'assets/MWStew.css': 'src/less/MWStew.less'
+				}
+			}
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			site: [
+				'assets/MWStew.css'
+			],
+		},
+		// cssjanus: {
+		// 	options: {
+		// 		generateExactDuplicates: true
+		// 	},
+		// 	site: {
+		// 		files: {
+		// 			'assets/MWStew.rtl.css': 'assets/MWStew.css'
+		// 		}
+		// 	}
+		// }
+	} );
+
+	// grunt.registerTask( 'default', [ 'less', 'csslint', 'cssjanus' ] );
+	grunt.registerTask( 'default', [ 'less', 'csslint' ] );
+};
--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require_once 'bootstrap.php';
 OOUI\Theme::setSingleton( new OOUI\MediaWikiTheme() );
 OOUI\Element::setDefaultDir( 'ltr' );
 $styles = array(
-	'includes/styles/ooui/oojs-ui-mediawiki.css',
-	'includes/styles/MWStew.css'
+	'assets/lib/ooui/oojs-ui-mediawiki.css',
+	'assets/MWStew.css'
 );
 ?>
 <html>
@@ -23,8 +23,6 @@ $styles = array(
 <!-- "Fork me on github" banner !-->
 <a href="https://github.com/mooeypoo/MWStew" class="mwstew-ui-forkgithub"><img style="top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 
-
-	<div class='wrapper'>
 <?php
 	// Title
 	$title = new OOUI\LabelWidget( array(
@@ -269,15 +267,14 @@ foreach ( $hooks as $hook => $desc ) {
 
 	$form->addItems( array( $submitFieldsetLayout ) );
 ?>
+	<div class='wrapper'>
 	<div class="mwstew-ui-title">
 <?php
 	echo $title;
 	echo $subtitle;
 ?>
 	</div>
-<?php
-	echo $form;
-?>
+		<?php echo $form; ?>
 	</div>
 </body>
 </html>
