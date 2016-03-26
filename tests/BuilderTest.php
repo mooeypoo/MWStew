f16d6d1a90fe982d854cdf2bde0694ec11d0fdd1
Moriel Schottlender <moriel@gmail.com>
2016-03-26 00:32:46 -0700

    Trying another fix for .travis.yml phpunit version failure

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,5 +1,4 @@
 <?php
-require_once "bootstrap.php";
 
 class BuilderTest extends PHPUnit_Framework_TestCase {
 
--- tests/SanitizerTest.php
+++ tests/SanitizerTest.php
@@ -1,5 +1,4 @@
 <?php
-require_once "bootstrap.php";
 
 class SanitizerTest extends PHPUnit_Framework_TestCase {
 
